1fdc3c260f44facca086c7eb76a0603fd7944a02
gabriel.eric <gabriel.eric@ufrpe.br>
2025-01-19 02:00:55 +0000

    Teste

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -6,4 +6,9 @@ def home(request):
     if request.method == "GET":
         return render(request, 'mysite/home/templates/index.html')
     else:
-        return HttResponse("ERROR")
+        action == request.POST.get('action')
+        if action == "login":
+            return HttpResponse("Página de login")
+        else:
+            return HttpResponse("Página de registro")
+
