e73d54d8758f4a706679230aa63a7e460252cabf
gabriel.eric <gabriel.eric@ufrpe.br>
2025-01-21 06:01:53 +0000

    Front end

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     if request.method == "GET":
-        return render(request, 'mysite/home/templates/index.html')
+        return render(request, 'home/index.html')
     else:
         action == request.POST.get('action')
         if action == "login":
